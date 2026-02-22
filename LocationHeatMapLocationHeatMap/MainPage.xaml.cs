2165c898148ca4a2865f4d5d4f0133dc0871f8ef
Murali Krishna Chintha <mchintha34546@ucumberlands.edu>
2026-02-21 22:04:22 -0600

    Add files via upload

---
+++ LocationHeatMapLocationHeatMap/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Maui.Controls.Maps;
+using Microsoft.Maui.Maps;
+
+namespace LocationHeatMap;
+
+public partial class MainPage : ContentPage
+{
+    private readonly MainViewModel _vm;
+
+    public MainPage(MainViewModel vm)
+    {
+        InitializeComponent();
+        _vm = vm;
+        BindingContext = _vm;
+
+        // Default view (will auto move to last point when data exists)
+        HeatMap.MoveToRegion(MapSpan.FromCenterAndRadius(new Location(37.3349, -122.0090), Distance.FromKilometers(3)));
+    }
+
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+
+        await _vm.InitializeAsync();
+        RenderHeat();
+
+        _vm.HeatElements.CollectionChanged += (_, __) => RenderHeat();
+    }
+
+    private void RenderHeat()
+    {
+        HeatMap.MapElements.Clear();
+
+        foreach (var el in _vm.HeatElements)
+            HeatMap.MapElements.Add(el);
+
+        // Auto zoom to latest point
+        if (_vm.LastLocation != null)
+        {
+            HeatMap.MoveToRegion(MapSpan.FromCenterAndRadius(
+                new Location(_vm.LastLocation.Latitude, _vm.LastLocation.Longitude),
+                Distance.FromKilometers(2)));
+        }
+    }
+}
---
+++ LocationHeatMapLocationHeatMap/MauiProgram.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace LocationHeatMap;
+
+public static class MauiProgram
+{
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+
+        builder
+            .UseMauiApp<App>()
+            .UseMauiMaps()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            });
+
+
+
+        // DI
+        builder.Services.AddSingleton<LocationRepository>();
+        builder.Services.AddSingleton<LocationTracker>();
+        builder.Services.AddSingleton<MainViewModel>();
+        builder.Services.AddSingleton<MainPage>();
+
+        return builder.Build();
+    }
+}
